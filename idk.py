609d1dd27e17b12671bd45a817425dd6eba89ffe
JustMurka <oraclevm2021@gmail.com>
2024-12-29 11:34:26 +0700

    asdasd

---
+++ idk.py
@@ -0,0 +1,22 @@
+import colorama
+import os
+import random
+from colorama import Fore, Style
+
+a = Fore.RED + "o" + Style.RESET_ALL
+
+print(f"""
+          *
+         /_\\
+        /{a}_{a}\\
+       /_{a}_{a}_\\
+      /_{a}__{a}_{a}\\
+     /{a}___{a}___{a}\\
+    /__{a}_____{a}__\\
+   /{a}__{a}___{a}____{a}\\
+         |||      
+""")
+
+print(Style.RESET_ALL)
+
+
